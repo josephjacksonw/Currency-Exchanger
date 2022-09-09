505d1ad7fa56106311ac9d07c2bf6f450fcef1cc
josephjacksonw <josephjacksonw@gmail.com>
2022-09-09 19:36:09 -0400

    error check now outputs errors

--- src/CurrencyService.js
+++ src/CurrencyService.js
@@ -3,7 +3,7 @@ export default class CurrencyService {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
-          const errorMessage = `error response stuff with $ {}`;
+          const errorMessage = `${response.status} ${response.statusText}`;
           throw new Error(errorMessage);
         } else {
           return response.json();
@@ -14,3 +14,45 @@ export default class CurrencyService {
       });
   }
 }
+
+// export default class CurrencyService {
+//   static getExchanges() {
+//     return fetch(`https://v6.exchangerate-api.com/v6/12/latest/USD`)
+//       .then(function(response) {
+//         if (!response.ok) {
+//           const errorMessage = `${response.status} ${response.statusText}`;
+//           throw new Error(errorMessage);
+//         } else {
+//           return response.json();
+//         }
+//       })
+//       .catch(function(error) {
+//         return error;
+//       });
+//   }
+// }
+
+
+//so this one checked to see if I could just check the output.result is error but I think I can do a different way
+
+// export default class CurrencyService {
+//   static getExchanges() {
+//     return fetch(`https://v6.exchangerate-api.com/v6/latest/USD`)
+//       .then(function(response) {
+//         if (response.result === "error") {
+//           console.log("hey this error check works")
+//         } else {
+//           console.log(response.result)
+//         }
+//         if (!response.ok) {
+//           const errorMessage = `error response stuff with $ {}`;
+//           throw new Error(errorMessage);
+//         } else {
+//           return response.json();
+//         }
+//       })
+//       .catch(function(error) {
+//         return error;
+//       });
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -8,48 +8,71 @@ import CurrencyService from './CurrencyService';
 function pullCurrencies(request) {
   CurrencyService.getExchanges()
     .then(function(response) {
-      console.log(response)
-      //so everything below this probably won't work bc its for an array being returned
-      printResponse(response, request)
-      if(response.length > 0) {
-        printCurrencies(response, request);
+      // console.log(response)
+      // //so everything below this probably won't work bc its for an array being returned
+      // console.log(typeof(response))
+      // console.log(response.Error)
+      // console.log("the next one says error if its an error")
+      // console.log(response.name)
+      if (response.name === "Error") {
+        console.log("this error check worked")
+        console.log(response)
+        printError(response, request)
       } else {
-        printError(response, request);
+        printCurrencies(response, request)
       }
+      // console.log(response.message)
+      // console.log(response.cause)
+      // console.log(response.toString()) // this one will output it
+      // printResponse(response, request)
+      
+      // if (response.typeof() === "string") {
+      //   console.log("hey this error check works")
+      // }
+      // if(response.length > 0) {
+      //   printCurrencies(response, request);
+      // } else {
+      //   printError(response, request);
+      // }
     });
 }
 
 //User Logic
-function printResponse(response, request) {
-  // let output = document.querySelector("output");
-  let p = document.createElement("p");
-  p.innerText = request;
-  console.log(response)
-  console.log(request)
-  console.log(p)
-  // console.log(output)
-  // output.appendChild(p);
-  let p2 = document.createElement("p");
-  p2.innerText = "your conversion" + response.conversion_rates.AED;
-  // output.appendChild(p2);
-  document.getElementById("outputs").appendChild(p);
-  document.getElementById("outputs").appendChild(p2);
-}
+// function printResponse(response, request) {
+//   // let output = document.querySelector("output");
+//   let p = document.createElement("p");
+//   p.innerText = request;
+//   console.log(response)
+//   console.log(request)
+//   console.log(p)
+//   // console.log(output)
+//   // output.appendChild(p);
+//   let p2 = document.createElement("p");
+//   p2.innerText = "your conversion" + response.conversion_rates.AED;
+//   // output.appendChild(p2);
+//   document.getElementById("outputs").appendChild(p);
+//   document.getElementById("outputs").appendChild(p2);
+// }
 
 
 function printCurrencies(response, request) {
   console.log("printCurrnecies");
   console.log(response, request);
+  let p = document.createElement("p");
+  p.innerText = "$" + request;
+  document.getElementById("outputs").appendChild(p);
 }
 
 function printError(response, request) {
   console.log("printError");
   console.log(response, request);
+  let p = document.createElement("p")
+  p.innerText = response
+  document.getElementById("outputs").appendChild(p)
 }
 
 function handleForm(e) {
   e.preventDefault();
-  console.log(pullCurrencies);
   const usd = document.querySelector("#usd").value;
   pullCurrencies(usd)
 }
@@ -60,6 +83,13 @@ window.addEventListener("load", function() {
 
 /*
 
+So right now, they can put in a value. when they hit submit I take that number and call the api to bring up the conversion api. The error check now sends the error to the right spot along with the error code
+
+I need to make sure that the function then outputs the amount they put in
+Then I need to have something check to see if there is anything checked in the radio form. 
+if there isn't then it will say to select a currency to convert it to
+if there is then it just shows what it is
+  I can probably just have it as hidden text that always changes based on whether there is somehting selected or not
 
 
 
@@ -80,7 +110,7 @@ Users should be able to convert U.S. currency into at least 5 other types of cur
 If the API call results in an error (any message not a 200 OK), the application should return a notification to the user that states what the error is. (That means the error should show up in the DOM, not in the console.) GOTTA FIND A WAY TO MAKE AN ERROR i GUESS
 
 
-If the query response doesn't include that particular currency, the application should return a notification that states the currency in question doesn't exist. (Note: Even if you use a dropdown menu to specify currencies instead of a form field, you'll still need to add this functionality to your code.) OK SO LIKE IDK HOW i WOULD BUILD THIS LMAO. ILL MESS AROUND I GUESS. OK SO I 
+If the query response doesn't include that particular currency, the application should return a notification that states the currency in question doesn't exist. (Note: Even if you use a dropdown menu to specify currencies instead of a form field, you'll still need to add this functionality to your code.) OK SO LIKE IDK HOW i WOULD BUILD THIS LMAO. ILL MESS AROUND I GUESS. OK SO when they submit the money they have, I output their currecnty and say what that would be
 
 
 */
